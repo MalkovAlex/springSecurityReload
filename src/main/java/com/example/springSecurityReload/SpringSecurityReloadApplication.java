407d5317b5cc90036b34567790f73a9cf0e69bf2
MalkovAlex <amalkov681@gmail.com>
2023-03-08 17:30:53 +0500

    first commit

---
+++ src/main/java/com/example/springSecurityReload/SpringSecurityReloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springSecurityReload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springSecurityReload/SpringSecurityReloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springSecurityReload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
